c7610b9830ac4317f189357a1791c9eb6430b0fa
linus1201 <linusiddi4@gmail.com>
2024-01-06 11:10:43 +0000

    single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print single numbers in base 10 starting for 0
+ * followed by a new line.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int x = 0;
+for (x = 0; x <= 9; x++)
+{
+printf("%d", x);
+}
+printf("\n");
+return (0);
+}
