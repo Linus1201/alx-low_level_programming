af3b8af47472201375002901c784660fe73ad567
linus1201 <linusiddi4@gmail.com>
2024-01-08 22:18:39 +0000

    new commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,23 @@
-#include <stdio.h>
-/**
- * main - program will print _putchar
- * followed by a new line
- *Return: Always 0 (Success)
+#include "main.h"
 
+/**
+ * main - prints _putchar
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char text[9] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
