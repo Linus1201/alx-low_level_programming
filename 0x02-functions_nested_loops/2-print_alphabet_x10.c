24b614febad5e27c2bf54ce94b45dbf1b0cab374
linus1201 <linusiddi4@gmail.com>
2024-01-13 12:07:24 +0000

    new

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - print alphabet in lowercase 10 times
+ * main - print alphabet in lowercase
  *
- * print_alphabet_x10 - print alphabet 10 times
+ * print_alphabet_x10 - prints alphabet in lowercase 10 times
  *
  * Return: void
  */
