5e15894d6c7928eba9b2395e483fcd74d7b69c65
linus1201 <linusiddi4@gmail.com>
2024-01-13 12:32:13 +0000

    returns alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for alphabets
+ *
+ * @c: is an ascii character
+ *
+ * Return: 1 if alphabet
+ */
+
+int _isalphabet(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
